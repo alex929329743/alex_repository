224663c28f07dd03604f39c66a48f5ec5a7629ec
alex929329743 <929329743@qq.com>
2017-02-21 10:52:13 +0800

    Add files via upload

---
+++ test.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+/**
+ * Created by Administrator on 2017/2/20 0020.
+ */
+public class test {
+    public static void main(String[] args){
+        System.out.println("hello World");
+    }
+
+}
